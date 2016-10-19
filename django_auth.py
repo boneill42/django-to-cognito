c75ca85c538ceaaea6209c29719b62a360fd2994
Brian O'Neill <bone@alumni.brown.edu>
2016-10-19 12:05:53 -0400

    Authentication working

--- django_auth.py
+++ django_auth.py
@@ -1,35 +1,56 @@
 from django.contrib.auth import hashers
+from django.conf import settings
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.databases.mysql import MySQLDialect
+from sqlalchemy.sql import text
 
-# root/django/trunk/django/contrib/auth/models.py
-def get_hexdigest(algorithm, salt, raw_password):
-    """
-    Returns a string of the hexdigest of the given plaintext password and salt
-    using the given algorithm ('md5', 'sha1' or 'crypt').
-    """
-    raw_password, salt = smart_str(raw_password), smart_str(salt)
-    if algorithm == 'crypt':
-        try:
-            import crypt
-        except ImportError:
-            raise ValueError('"crypt" password algorithm not supported in this environment')
-        return crypt.crypt(raw_password, salt)
+# ------------------------------------------------------------------------------------------
+# Database
+# ------------------------------------------------------------------------------------------
+dialect = MySQLDialect()
 
-    if algorithm == 'md5':
-        return md5_constructor(salt + raw_password).hexdigest()
-    elif algorithm == 'sha1':
-        return sha_constructor(salt + raw_password).hexdigest()
-    raise ValueError("Got unknown password algorithm type in password.")
+engine_defaults = {
+    'pool_size': 4,
+    'max_overflow': 0,
+    'pool_recycle': 300,
+    'pool_timeout': 30.0,
+}
 
-
-# Also from root/django/trunk/django/contrib/auth/models.py
-def check_password(raw_password, enc_password):
-    """
-    Returns a boolean of whether the raw_password was correct. Handles
-    encryption formats behind the scenes.
-    """
-    algo, salt, hsh = enc_password.split('$')
-    return hsh == get_hexdigest(algo, salt, raw_password)
+OPTIONS = {
+    'charset': 'utf8',
+    'use_unicode': True,
+}
 
 
-pw = 'pbkdf2_sha256$10000$azYoUEOTGFH6$6MBRUInq9QOD1sB4r2HwNzFzdweEYJ/bEHrOiFqTga0='
-print hashers.check_password('testpw001',pw)
+def none_if_empty(s):
+    return s if s else None
+
+
+def make_dsn(db_settings):
+    return str(URL(
+        'mysql',
+        none_if_empty(db_settings['USER']),
+        none_if_empty(db_settings['PASSWORD']),
+        none_if_empty(db_settings['HOST']),
+        none_if_empty(db_settings['PORT']),
+        none_if_empty(db_settings['NAME']),
+        OPTIONS))
+
+
+QUERY_PASSWORD_BY_USER = text(
+    "SELECT password "
+    "FROM auth_user "
+    "WHERE username = :username"
+).compile(dialect=dialect)
+
+# ------------------------------------------------------------------------------------------
+# Authentication
+# ------------------------------------------------------------------------------------------
+
+dsn = make_dsn(settings.DATABASES['default'])
+engine = create_engine(dsn, **engine_defaults)
+result = engine.execute(QUERY_PASSWORD_BY_USER, username='boneill@monetate.com')
+pw = result.fetchone()['password']
+authenticated = hashers.check_password('testpw00', str(pw))
+print ("Authenticated: [{}]".format(authenticated))
--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 export DJANGO_SETTINGS_MODULE='min_django_settings'
 export PYTHONPATH='/Users/brianoneill/'
